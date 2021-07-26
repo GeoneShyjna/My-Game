045accf842abe34c1339b94008534ebbd4397490
GeoneShyjna <83636512+GeoneShyjna@users.noreply.github.com>
2021-07-26 15:10:18 +1200

    Update mygame.py
    Up to the first door

--- mygame.py
+++ mygame.py
@@ -1,2 +1,23 @@
-start = int(input("Enter your name to start the game"))
- print ("Hi{}".format start)
+import random
+name = input("What is your name? ")
+# Here the user is asked to enter the name first
+
+print("Hi ", name, "you are stuck in a maze, there is multiple doors in the maze but you can only choose one door")
+
+print("Choose a door! ")
+
+door = input(" **Door 1**\n  **Door 2**\n   **Door 3**")
+
+
+
+door_car = ["yes", "no", "no"]
+random.shuffle(door_car)
+for i in range(3)
+x = 0
+x += 1
+door1 = []
+door2 = []
+door3 = []
+
+door(x).append(door_car.pop())
+
