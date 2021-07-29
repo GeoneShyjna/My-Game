7110d01b9a8e0f3a3989e9bfa9645ed65a7bd185
GeoneShyjna <83636512+GeoneShyjna@users.noreply.github.com>
2021-07-29 12:10:08 +1200

    MYgame
    Add level 1 into the main game

--- Mygame2.py
+++ Mygame2.py
@@ -2,39 +2,19 @@ Level_1 = ""
 while Level_1.lower() != "xxx":
 
     Level_1 = input("This is the level 1 \n"
-                    "**Door 1**\n**Door 2**\n**Door 3**\n").lower()
+                    "**DOOR 1**\n**DOOR 2**\n**DOOR 3**\n")
 
-    if Level_1 == "Door1" or Level_1 == "1":
+    if Level_1 == "DOOR1" or Level_1 == "1":
         print("You passed this level on to the next level!")
 
-    elif Level_1 == "Door2" or Level_1 == "2":
+    elif Level_1 == "DOOR2 " or Level_1 == "2":
         print("\n***YOU DIED***")
         quit()
 
-    elif Level_1 == "Door3" or Level_1 == "3":
+    elif Level_1 == "DOOR3 " or Level_1 == "3":
         print("\n***YOU DIED***")
         quit()
 
     else:
         print("Please answer the question"
-              "\n**ANSWER LIKE Door4 or 4")
-
-Level_2 = ""
-while Level_2.lower() != "xxx":
-    Level_2 = input("This is the level 1 \n"
-                    "**Door 1**\n**Door 2**\n**Door 3**").lower()
-
-    if Level_2 == "Door1" or Level_2 == "1":
-        print("You passed this level on to the next level!")
-
-    elif Level_2 == "Door2" or Level_2 == "2":
-        print("\n***YOU DIED***")
-        quit()
-
-    elif Level_2 == "Door3" or Level_2 == "3":
-        print("\n***YOU DIED***")
-        quit()
-
-    else:
-        print("Please answer the question"
-              "\n**ANSWER LIKE Door4 or 4")
+              "\n**ANSWER HAVE TO BE IN CAPITAL AND NO SPACE** OR JUST THE NUMBER")
--- mygame.py
+++ mygame.py
@@ -1,23 +1,25 @@
-import random
+
 name = input("What is your name? ")
 # Here the user is asked to enter the name first
+print("Hi", name, "you are stuck in a maze, there is multiple doors in the maze but you can only choose one door")
 
-print("Hi ", name, "you are stuck in a maze, there is multiple doors in the maze but you can only choose one door")
+Level_1 = ""
+while Level_1.lower() != "xxx":
 
-print("Choose a door! ")
+    Level_1 = input("This is the level 1 \n"
+                    "**DOOR 1**\n**DOOR 2**\n**DOOR 3**\n")
 
-door = input(" **Door 1**\n  **Door 2**\n   **Door 3**")
+    if Level_1 == "DOOR1" or Level_1 == "1":
+        print("You passed this level on to the next level!")
 
+    elif Level_1 == "DOOR2 " or Level_1 == "2":
+        print("\n***YOU DIED***")
+        quit()
 
+    elif Level_1 == "DOOR3 " or Level_1 == "3":
+        print("\n***YOU DIED***")
+        quit()
 
-door_car = ["yes", "no", "no"]
-random.shuffle(door_car)
-for i in range(3)
-x = 0
-x += 1
-door1 = []
-door2 = []
-door3 = []
-
-door(x).append(door_car.pop())
-
+    else:
+        print("Please answer the question"
+              "\n**ANSWER HAVE TO BE IN CAPITAL AND NO SPACE OR JUST THE NUMBER**")
